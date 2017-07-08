e0f1d32ab18e50541b736de9d42db98036897dc4
Yong Lin <gmale88@gmail.com>
2017-07-08 10:20:19 -0500

    Add contacts component

---
+++ components/contacts.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Contacts extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>hi</div>
+    );
+  }
+}
+
+export default Contacts;
--- entry.jsx
+++ entry.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Contacts from './components/contacts.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Contacts />, document.getElementById('root'));
